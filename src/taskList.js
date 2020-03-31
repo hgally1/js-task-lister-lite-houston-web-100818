57e1a64a93431777fe4036ffbfcc6486874adfeb
Heidi Gally <heidigally@gmail.com>
2020-03-31 15:42:45 -0500

    js-task-lister-lite FINISHED

--- src/taskList.js
+++ src/taskList.js
@@ -1,2 +1,16 @@
 class TaskList {
+    constructor () {
+        this.tasks = [];
+    }
+    createNewTaskList(description, priority) {
+        const newTask = new Task(description, priority);
+        this.tasks.push(newTask);
+    }
+    renderTasks(){
+        return this.tasks.map((task) => task.render()).join("");
+    }
+    
+    deleteTask(description){
+        this.tasks = this.tasks.filter((task) => task.description !== description);
+    }
 }
